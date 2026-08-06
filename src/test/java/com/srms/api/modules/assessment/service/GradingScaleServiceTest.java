ed3c4a661552a13146ead88a21f74e7c1cca49e3
chrimztech <chrishentmatakala@yahoo.com>
2026-08-06 09:45:48 +0200

    feat(tests): update grading scale boundaries and descriptions for Zambia 2023

--- src/test/java/com/srms/api/modules/assessment/service/GradingScaleServiceTest.java
+++ src/test/java/com/srms/api/modules/assessment/service/GradingScaleServiceTest.java
@@ -14,32 +14,33 @@ class GradingScaleServiceTest {
 
     @Test
     void mapsEveryBoundaryToTheZambia2023AchievementGrade() {
-        assertGrade(100, "A");
-        assertGrade(80, "A");
-        assertGrade(79.99, "B+");
-        assertGrade(79, "B+");
-        assertGrade(70, "B+");
-        assertGrade(69.5, "B");
-        assertGrade(69, "B");
-        assertGrade(60, "B");
-        assertGrade(59, "C+");
-        assertGrade(50, "C+");
-        assertGrade(49, "C");
-        assertGrade(40, "C");
-        assertGrade(39, "D+");
-        assertGrade(30, "D+");
-        assertGrade(29, "D");
-        assertGrade(20, "D");
-        assertGrade(19.99, "E");
-        assertGrade(19, "E");
-        assertGrade(0, "E");
+        assertGrade(100, "1");
+        assertGrade(75, "1");
+        assertGrade(74.99, "2");
+        assertGrade(74, "2");
+        assertGrade(70, "2");
+        assertGrade(69.5, "3");
+        assertGrade(69, "3");
+        assertGrade(65, "3");
+        assertGrade(64, "4");
+        assertGrade(60, "4");
+        assertGrade(59, "5");
+        assertGrade(55, "5");
+        assertGrade(54, "6");
+        assertGrade(50, "6");
+        assertGrade(49, "7");
+        assertGrade(45, "7");
+        assertGrade(44, "8");
+        assertGrade(40, "8");
+        assertGrade(39, "9");
+        assertGrade(0, "9");
     }
 
     @Test
     void preservesDescriptionsAndPoints() {
         GradingBandDto band = service.evaluate(bands, 75);
-        assertEquals("Very Good", band.getDescription());
-        assertEquals(2, band.getPoints());
+        assertEquals("DISTINCTION", band.getDescription());
+        assertEquals(1, band.getPoints());
     }
 
     private void assertGrade(double percentage, String expected) {
